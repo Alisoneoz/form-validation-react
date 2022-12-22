0d1fdbc4f26c270812279e126caf47c122bb90db
Alison Estephany <alisonestephanyoz@gmail.com>
2022-12-21 20:12:08 -0400

    Add form and style with Tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,82 @@
+import { useState } from "react";
 import FormInput from "./components/FormInput";
 
 function App() {
-  return (
-    <div className="App flex items-center justify-center h-screen ">
-      <form>
-        <FormInput  placeholder='Username'/>
-        <FormInput  placeholder='youremail@company.com'/>
-        <FormInput  placeholder='Full Name'/>
-        <FormInput  placeholder='argo mais'/>
+  const [ values, setValues] = useState({
+    userName:'',
+    email:'',
+    birthday:'',
+    password:'',
+    confirmPassword:''
+  })
 
+  //props of the inputs of the form
+  const inputs = [
+    {
+      id:1,
+      name:'userName',
+      type:'text',
+      placeholder:'Username',
+      errorMessage:"",
+      label:'Username'
+    },
+    {
+      id:2,
+      name:'email',
+      type:'email',
+      placeholder:'youremail@company.com',
+      errorMessage:"",
+      label:'Email'
+    },
+    {
+      id:3,
+      name:'birthday',
+      type:'date',
+      placeholder:'Birthday',
+      errorMessage:"",
+      label:'Birthday'
+    },
+    {
+      id:4,
+      name:'password',
+      type:'password',
+      placeholder:'password',
+      errorMessage:"",
+      label:'Password'
+    },
+    {
+      id:5,
+      name:'confirmPassword',
+      type:'password',
+      placeholder:'Confirm your password',
+      errorMessage:"",
+      label:'Confirm your password'
+    }
+  ]
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const onChange = (e)=> {
+    setValues({...values, [e.target.name]: e.target.value})
+  }
+
+  console.log(values);
+  return (
+    <div className="app flex justify-center min-h-screen items-center">
+      <form onSubmit={handleSubmit} className="bg-gray-50/50 p-7 border-none rounded-lg h-3/4 w-2/3 sm:w-1/2 xl:w-1/3">
+      <h2 className="text-center font-bold text-md sm:text-2xl pb-2">Register</h2>
+       {inputs.map((input) => { 
+          return(
+            <FormInput 
+            key={input.id}
+            {...input}
+            value={values[input]}
+            onChange={onChange}
+          />
+          )
+       })}
+      <button className="w-full  p-1 border rounded-lg justify-center bg-purple-900 font-bold text-white shadow-xl">Submit</button>
       </form>
     </div>
   );
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 
 const FormInput = (props) => {
+  
+  //destructuring props to have a more concise code
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
   return (
-    <div>
-      <div className="block">
-        <label></label>
-        <input type={props} placeholder={props.placeholder} className="mx-0 my-2 p-4 border rounded-md"/>
-      </div>
+    <div className='flex flex-col '>
+      
+      <label 
+        className='font-semibold text-xs sm:text-base'>{label}
+      </label>
+       
+       <input 
+          {...inputProps}
+          onChange={onChange}
+          className="mx-0 mt-1 mb-4 p-2 border rounded-md focus:border-slate-500 text-xs sm:text-sm w-full "
+        />
+      <span>{errorMessage}</span> 
     </div>
   )
 }
 
 export default FormInput
+//imported into app.js
