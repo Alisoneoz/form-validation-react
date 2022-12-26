fe104c0cafccb7fb60ef0986dc23b760d5337b6b
Alison Estephany <alisonestephanyoz@gmail.com>
2022-12-26 11:23:38 -0400

    Add validation functionality and style

--- src/App.js
+++ src/App.js
@@ -17,15 +17,19 @@ function App() {
       name:'userName',
       type:'text',
       placeholder:'Username',
-      errorMessage:"",
-      label:'Username'
+      errorMessage:"Username should be 3-16 characters, shouldn't include any special character",
+      label:'Username',
+      pattern:"^[A-Za-z0-9]{3,16}$",
+      title:"Username should be 8-16 characters, shouldn't includeany special character",
+      required:true,
+
     },
     {
       id:2,
       name:'email',
       type:'email',
       placeholder:'youremail@company.com',
-      errorMessage:"",
+      errorMessage:"Should be a valid email address",
       label:'Email'
     },
     {
@@ -41,16 +45,18 @@ function App() {
       name:'password',
       type:'password',
       placeholder:'password',
-      errorMessage:"",
-      label:'Password'
+      errorMessage:"Password should be 8-20 characters",
+      label:"Password",
+      pattern:"^[A-Za-z0-9]{8,20}$",
     },
     {
       id:5,
       name:'confirmPassword',
       type:'password',
       placeholder:'Confirm your password',
-      errorMessage:"",
-      label:'Confirm your password'
+      errorMessage:"Passwords don't match!",
+      label:'Confirm your password',
+      pattern:values.password
     }
   ]
   const handleSubmit = (e) => {
@@ -65,13 +71,13 @@ function App() {
   return (
     <div className="app flex justify-center min-h-screen items-center">
       <form onSubmit={handleSubmit} className="bg-gray-50/50 p-7 border-none rounded-lg h-3/4 w-2/3 sm:w-1/2 xl:w-1/3">
-      <h2 className="text-center font-bold text-md sm:text-2xl pb-2">Register</h2>
+      <h2 className="text-center font-bold text-lg sm:text-2xl pb-2">Register</h2>
        {inputs.map((input) => { 
           return(
             <FormInput 
             key={input.id}
             {...input}
-            value={values[input]}
+            value={values[input.name]}
             onChange={onChange}
           />
           )
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./formInput.css";
 
 const FormInput = (props) => {
   
   //destructuring props to have a more concise code
-  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  const { label, errorMessage, onChange, required, id, ...inputProps } = props;
+
+  //state to update when the input is focused in order to display error messages only after user filled the field
+  const [focused, setFocused]= useState(false)
+
+  const handleFocus = (e) => {
+    setFocused(true)
+  }
 
   return (
     <div className='flex flex-col '>
       
-      <label 
-        className='font-semibold text-xs sm:text-base'>{label}
-      </label>
-       
-       <input 
-          {...inputProps}
-          onChange={onChange}
-          className="mx-0 mt-1 mb-4 p-2 border rounded-md focus:border-slate-500 text-xs sm:text-sm w-full "
-        />
-      <span>{errorMessage}</span> 
+      <div className="mb-1">
+        <label 
+          className='font-semibold text-base sm:text-lg'>{label}
+        </label>
+      </div>
+      <div className="mb-4">
+        <input 
+            {...inputProps}
+            onChange={onChange}
+            required
+            onBlur={handleFocus}
+            focused={focused.toString()}
+            className="mx-0  p-2 border rounded-md focus:border-slate-500 text-sm sm:text-base w-full peer"
+          />
+        <span className='mt-1 font-semibold text-red-600 text-xs sm:text-sm leading-3 hidden  '>{errorMessage}</span> 
+      </div>
+
     </div>
   )
 }
